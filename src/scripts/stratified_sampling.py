121582f854ec6178d2b73fbda1cccbc5ba364cf2
juaandominguez <juan.dominguezr@udc.es>
2024-07-22 21:22:09 +0200

    feat(scripts): Implement stratified sampling for TREC relevance judgments

---
+++ src/scripts/stratified_sampling.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+FILE_NAME = 'data/raw/robust04/qrels.robust04.300-450.600-700.trec.txt'
+NUM_OF_SAMPLES = 10
+OUTPUT_FILE = 'data/processed/stratified_sampling_pairs.txt'
+
+df = pd.read_csv(FILE_NAME, sep=' ', header=None, usecols=[0, 2, 3])
+df.columns = ['TOPIC', 'DOCUMENT', 'RELEVANCE']
+
+df = df.groupby('RELEVANCE', group_keys=False).apply(lambda x: x.sample(NUM_OF_SAMPLES))
+
+df.to_csv(OUTPUT_FILE ,sep=' ', header=None, index=None)
