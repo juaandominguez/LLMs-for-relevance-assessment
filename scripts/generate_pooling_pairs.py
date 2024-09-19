f80914f84fd392b579eef2cc1f746f218cc692ef
juaandominguez <juandominguezrguez@gmail.com>
2024-09-19 22:00:12 +0200

    feat(scripts): Add pooling script

--- scripts/generate_pooling_pairs.py
+++ scripts/generate_pooling_pairs.py
@@ -1,11 +1,22 @@
 import pandas as pd
+import os
 
 POOLING_DEPTH = 100
+RUNS_PATH = '/mnt/runs/students/juan.dominguezr/TFG/data/raw/robust04/runs'
+QREL_PATH = '/mnt/runs/students/juan.dominguezr/TFG/data/raw/robust04/qrels.robust04.300-450.600-700.trec.txt'
+OUTPUT_FILE = '/mnt/runs/students/juan.dominguezr/TFG/data/processed/pooling_pairs.csv'
 
-df = pd.read_csv('qrels.robust04.300-450.600-700.trec.txt', sep=' ', header=None, usecols=[0,2,3], names=['query_id', 'doc_id', 'relevance'])
+run_dfs = []
 
-grouped = df.groupby('query_id', group_keys=False).apply(
-    lambda x: x.sort_values(by='relevance', ascending=False).head(POOLING_DEPTH)
-).reset_index(drop=True)
+for file in os.listdir(RUNS_PATH):
+    df = pd.read_csv(f'{RUNS_PATH}/{file}', sep='\s+', header=None, usecols=[0,2], names=['query_id', 'doc_id'])
+    grouped = df.groupby('query_id', group_keys=False).head(POOLING_DEPTH).reset_index(drop=True)
+    run_dfs.append(grouped)
 
-grouped.to_csv('data/processed/pooling_prompts.csv', sep=' ', header=None, index=None)
+df = pd.concat(run_dfs, ignore_index=True).drop_duplicates()
+
+qrel_df = pd.read_csv(QREL_PATH, sep=' ', header=None, usecols=[0,2,3], names=['query_id', 'doc_id', 'relevance'])
+
+merged_df = pd.merge(df, qrel_df, on=['query_id', 'doc_id'])
+
+merged_df.to_csv(OUTPUT_FILE, sep=' ',  index=None, header=None)
