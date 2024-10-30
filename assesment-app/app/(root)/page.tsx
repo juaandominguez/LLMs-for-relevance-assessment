7d56c8f2d0e2d3d992f2dd9ddbc7f618cc6e90a3
juaandominguez <juandominguezrguez@gmail.com>
2024-10-30 20:48:52 +0100

    feat(app): Add app initial config and guidelines

---
+++ assesment-app/app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import PromptInfo from "./components/guidelines";
+export default function Home() {
+
+  return (
+    <main className="w-[100dvw] h-[100dvh] flex items-center justify-center">
+      <PromptInfo />
+    </main>
+  );
+}
