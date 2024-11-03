29f67fb8b744e483c6f432165cd5377550879e4b
juaandominguez <juandominguezrguez@gmail.com>
2024-11-03 18:35:34 +0100

    fix(app): Fix authentication dependencies

--- assesment-app/auth.ts
+++ assesment-app/auth.ts
@@ -8,19 +8,9 @@ import { db } from "./db/schema";
 import { getUser, createUser, createGuestUser } from "./db/queries";
 
 declare module "next-auth" {
-  /**
-   * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   */
   interface Session {
     user: {
-      /** The user's postal address. */
       id: string;
-      /**
-       * By default, TypeScript merges new interface properties and overwrites existing ones.
-       * In this case, the default session user properties will be overwritten,
-       * with the new ones defined above. To keep the default session user properties,
-       * you need to add them back into the newly declared interface.
-       */
     } & DefaultSession["user"];
   }
 }
