a45af9df8c4ad1baff5cc0fcab5290317f410061
juaandominguez <juandominguezrguez@gmail.com>
2024-11-05 21:20:57 +0100

    fix(navbar): Remove border

--- assesment-app/components/navbar.tsx
+++ assesment-app/components/navbar.tsx
@@ -5,7 +5,7 @@ import { auth } from "@/auth"
 const Navbar = async () => {
     const session = await auth()
     return (
-        <nav className='flex w-full justify-between min-h-[5dvh] border border-red-600 items-center px-[15dvw] md:px-[10dvw]'>
+        <nav className='flex w-full justify-between min-h-[5dvh] items-center px-[15dvw] md:px-[10dvw]'>
             <div>LOGO</div>
             <NavbarAvatar user={session?.user as User} />
         </nav>
