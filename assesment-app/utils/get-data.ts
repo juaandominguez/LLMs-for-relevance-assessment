72189fb1a1ac2dca827ea016fdf8c1896af31568
juaandominguez <juandominguezrguez@gmail.com>
2024-11-17 19:48:30 +0100

    chore(app): Modify data extraction

--- assesment-app/utils/get-data.ts
+++ assesment-app/utils/get-data.ts
@@ -1,14 +1,15 @@
-import topics from "../data/topics.json";
-import documents from "../data/documents.json";
-export const getTopicAndDocument = (topicId: number, documentId: string) => {
-  const topic = topics.find((topic) => topic.id === topicId);
-  const doc = documents.find((document) => document.id === documentId);
-  if (!topic) {
-    throw new Error(`Topic with id ${topicId} not found`);
+import docs_db from "@/data/docs_db.json";
+export const getTopicAndDocument = (pairId: number) => {
+  const pair = docs_db.find((doc) => doc.pair_id === pairId);
+  if (!pair) {
+    throw new Error(`Pair with id ${pairId} not found`);
   }
-  if (!doc) {
-    throw new Error(`Document with id ${documentId} not found`);
-  }
-  const docHtml = doc.html;
-  return { topic, docHtml };
+  const doc = pair.document;
+  const topic = {
+    title: pair.query_title,
+    description: pair.query_description,
+    narrative: pair.query_narrative,
+  };
+
+  return { topic, doc };
 };
