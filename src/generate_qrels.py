4e9bd196c7ba954a0d3db40fb8d7a8bd35db4d7f
juaandominguez <juandominguezrguez@gmail.com>
2025-03-29 18:13:20 +0100

    feat(src): Add script to generate qrels

---
+++ src/generate_qrels.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from models.assessors.factory import AssessorFactory, AssessorType
+from io import StringIO
+
+PROMPTS_PATH = 'data/processed/all_prompts.jsonl'
+OUTPUT_FILE = 'data/processed/qrels-by-llm-8b.txt'
+
+assesor = AssessorFactory().create_assessor(AssessorType.LLAMA)
+
+
+cnt = 0
+with open(OUTPUT_FILE, 'a') as f:
+    with open(PROMPTS_PATH, 'r') as file:
+        for line in file:
+            try:
+                cnt += 1
+                if(cnt <= 268): continue
+                df = pd.read_json(StringIO(line), lines=True)
+                print(f"Line {cnt}", flush=True)
+                prompt = df['prompt'].iloc[0]
+                relevance = df['relevance'].iloc[0]
+                query_id = df['query_id'].iloc[0]
+                doc_id = df['doc_id'].iloc[0]
+
+                response = assesor.assess(prompt, relevance)
+                if response is not None:
+                    if int(response) >= 2:
+                        response = 2
+                    f.write(f'{query_id} 0 {doc_id} {response}\n')
+                else:
+                    f.write(f'{query_id} 0 {doc_id} 0\n')
+                    print(f"Error in response generation for prompt {cnt}")
+
+            except Exception as e:
+                print(f"Error in response generation for prompt {cnt}, {e}")
