1c28c97bede2812870bc67e2ba1a4b07d78def3b
juaandominguez <juandominguezrguez@gmail.com>
2025-04-27 16:45:01 +0200

    fix(CI/CD): Fix e2e test integration

--- assessment-app/playwright.config.ts
+++ assessment-app/playwright.config.ts
@@ -60,9 +60,9 @@ export default defineConfig({
     // },
   ],
 
-  webServer: {
-    command: "bun run start",
-    url: "http://127.0.0.1:3000",
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: "bun run start",
+  //   url: "http://127.0.0.1:3000",
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
